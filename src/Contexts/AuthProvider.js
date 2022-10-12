ce075bd9b41a3778879b25d2ce2ded5e96a9e1e6
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2022-10-12 18:05:16 -0500

    Persistencia de sesion - beta

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,26 +1,59 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
 export const authContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({ token: null, isLogged:false, uuid:null});
-  const [userAndPassword, setUserAndPassword] = useState({user:'', password:'', verificationCode:''})
+  const [auth, setAuth] = useState({
+    token: null,
+    isLogged: false,
+    uuid: null,
+  });
+  const [userAndPassword, setUserAndPassword] = useState({
+    user: "",
+    password: "",
+    verificationCode: "",
+  });
 
   useEffect(() => {
-        console.log(auth)
-  }, [auth])
-     
+    console.log(auth)
+  }, [auth]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "Session",
+      JSON.stringify({ auth: auth, user: userAndPassword.user })
+    );
+    console.log(JSON.parse(localStorage.getItem("Session")));
+    const session = JSON.parse(localStorage.getItem("Session"));
+    if (session) {
+      setAuthData({token: session.auth.token, isLogged: session.auth.isLogged, uuid: session.auth.uuid});
+      // setUserAndPassword({...userAndPassword, user: session.user});
+    }
+  }, []);
+  
 
   const setAuthData = (token, isLogged, uuid) => {
-    setAuth({token: token, isLogged:isLogged, uuid:uuid});
+    setAuth({ token: token, isLogged: isLogged, uuid: uuid });
   };
 
-  const setUserPassword = (userPassword) =>{
-    setUserAndPassword(userPassword)
-  }
+  const setUserPassword = (userPassword) => {
+    setUserAndPassword(userPassword);
+  };
+
+  const logOut = () => {
+    setAuthData({ token: null, isLogged: false, uuid: null });
+    setUserAndPassword({
+      user: "",
+      password: "",
+      verificationCode: "",
+    });
+    localStorage.removeItem('Session');
+  };
 
   return (
-    <authContext.Provider value={{ auth, setAuthData, userAndPassword, setUserPassword}}>
+    <authContext.Provider
+      value={{ auth, setAuthData, userAndPassword, setUserPassword, logOut }}
+    >
       {children}
     </authContext.Provider>
   );
