bce8bc3ab81c60489ab83c1bd8014f3c2be76c38
Axel Solorzano 12 <83233607+axelsolorzano@users.noreply.github.com>
2022-10-19 16:35:36 -0500

    i modify the file .tsx and .css in the folder Buttons

--- src/Screens/TestScreen/index.tsx
+++ src/Screens/TestScreen/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import '../../../src/Components/Buttons/Buttons.css'
+import {ButtonPrimarys} from '../../../src/Components/Buttons/Buttons'
+
+interface buttonprimarysProps {
+  textButton: string;
+  isCancel: boolean;
+  isborder: boolean;
+  buttonOnClick: () => void
+}
+
 
 export const TestScreen = () => {
   return (
-    <div>testScreen</div>
+    <div className='contenedortest'><span>testScreen</span>
+      <ButtonPrimarys textButton={'TEXT'} isCancel={false} isborder={false} buttonOnClick={function (): void {} } />
+      <ButtonPrimarys textButton={'TEXT'} isCancel={false} isborder={true}  buttonOnClick={function (): void {} } />
+      <ButtonPrimarys textButton={'TEXT'} isCancel={true}  isborder={false} buttonOnClick={function (): void {} } />
+      <ButtonPrimarys textButton={'TEXT'} isCancel={true}  isborder={true}  buttonOnClick={function (): void {} } />
+    </div> 
   )
 }
