c752e2048e97158eb9fd54695d27a9baa862835c
David Sánchez <davidesj97@gmail.com>
2022-10-24 17:36:03 -0500

    Added function to open and close modal of new career

--- src/Screens/Administrativos/administradorAcademico/features/newCareer/modalNewCareer/test.tsx
+++ src/Screens/Administrativos/administradorAcademico/features/newCareer/modalNewCareer/test.tsx
@@ -1,45 +1,57 @@
-import { useState } from 'react';
-import { LogInContainer } from '../../../../../../Components/Containers/LogInContainer/logInContainer';
-import { TextInput } from '../../../../../../Components/TextInputs/TextInputs';
-import { PrimaryButton } from '../../../../../../Components/Buttons/Buttons';
+import { useState } from "react";
+import { LogInContainer } from "../../../../../../Components/Containers/LogInContainer/logInContainer";
+import { TextInput } from "../../../../../../Components/TextInputs/TextInputs";
+import { PrimaryButton } from "../../../../../../Components/Buttons/Buttons";
 
-import './index.css'
+interface ModalProps {
+  handleModal: () => void
+}
 
+interface CareerProps {
+  key: string;
+  officialKey: string;
+  shortName: string;
+  fullName: string;
+  grade: number;
+  level: number;
+}
 
-export const TestScreen = () => {
+export const Prueba = ({ handleModal }: ModalProps) => {
 
-  const [careerData, setCareerData] = useState({});
-  const [showModal, setShowModal] = useState(true);
+  const [careerData, setCareerData] = useState<CareerProps>({
+    key: '',
+    officialKey: '',
+    shortName: '',
+    fullName: '',
+    grade: 0,
+    level: 0,
+  });
 
-  const handleModal = () => {
-    setShowModal(!showModal)
-  }
-
-  const onChangeClave = (inputValue:string) => {
+  const onChangeClave = (key:string) => {
     setCareerData({
       ...careerData,
-      'key': inputValue,
+      key,
     })
   }
 
-  const onChangeClaveOficial = (inputValue:string) => {
+  const onChangeClaveOficial = (officialKey:string) => {
     setCareerData({
       ...careerData,
-      'officialKey': inputValue,
+      officialKey,
     })
   }
 
-  const onChangeShortName = (inputValue:string) => {
+  const onChangeShortName = (shortName:string) => {
     setCareerData({
       ...careerData,
-      'shortName': inputValue,
+      shortName,
     })
   }
 
-  const onChangeFullName = (inputValue:string) => {
+  const onChangeFullName = (fullName:string) => {
     setCareerData({
       ...careerData,
-      'fullName': inputValue,
+      fullName,
     })
   }
 
@@ -47,7 +59,7 @@ export const TestScreen = () => {
     const grade = Number(inputValue)
     setCareerData({
       ...careerData,
-      'grade': grade,
+      grade,
     })
   }
 
@@ -55,52 +67,55 @@ export const TestScreen = () => {
     const level = Number(inputValue)
     setCareerData({
       ...careerData,
-      'level': level,
+      level,
     })
   }
 
   const sendData = () => {
     console.log(careerData)
+    handleModal();
   }
 
   return (
-    (showModal ? <div className='modal_new-career'>
-      <LogInContainer>
-        <h2 className='title'>Nueva Carrera</h2>
+    <>
+      <div className='modal_new-career'>
+        <LogInContainer>
+          <h2 className='title'>Nueva Carrera</h2>
 
-          <div className='new-career_input'>
-            <label>Clave</label>
-            <TextInput textInputOnChange={onChangeClave} placeHolder='Escribe la clave'/>
-          </div>
-          <div className='new-career_input'>
-            <label>Clave oficial</label>
-            <TextInput textInputOnChange={onChangeClaveOficial} placeHolder='Escribe la clave'/>
-          </div>
-          <div className='new-career_input'>
-            <label>Nombre corto</label>
-            <TextInput textInputOnChange={onChangeShortName} placeHolder='Escribe el nombre corto'/>
-          </div>
-          <div className='new-career_input'>
-            <label>Nombre completo</label>
-            <TextInput textInputOnChange={onChangeFullName} placeHolder='Escribe el nombre completo'/>
-          </div>
-          <div className='new-career_input numbers_input'>
-            <div>
-              <label>Grado</label>
-              <TextInput textInputOnChange={onChangeGrade} placeHolder=''/>
+            <div className='new-career_input'>
+              <label>Clave</label>
+              <TextInput textInputOnChange={onChangeClave} placeHolder='Escribe la clave'/>
             </div>
-            <div>
-              <label>Nivel</label>
-              <TextInput textInputOnChange={onChangeLevel} placeHolder=''/>
+            <div className='new-career_input'>
+              <label>Clave oficial</label>
+              <TextInput textInputOnChange={onChangeClaveOficial} placeHolder='Escribe la clave'/>
             </div>
+            <div className='new-career_input'>
+              <label>Nombre corto</label>
+              <TextInput textInputOnChange={onChangeShortName} placeHolder='Escribe el nombre corto'/>
+            </div>
+            <div className='new-career_input'>
+              <label>Nombre completo</label>
+              <TextInput textInputOnChange={onChangeFullName} placeHolder='Escribe el nombre completo'/>
+            </div>
+            <div className='new-career_input numbers_input'>
+              <div>
+                <label>Grado</label>
+                <TextInput textInputOnChange={onChangeGrade} placeHolder=''/>
+              </div>
+              <div>
+                <label>Nivel</label>
+                <TextInput textInputOnChange={onChangeLevel} placeHolder=''/>
+              </div>
+            </div>
+
+
+          <div className="buttons">
+            <PrimaryButton buttonOnClick={handleModal} textButton='Cancelar' />
+            <PrimaryButton buttonOnClick={sendData} textButton='Guardar' />
           </div>
-
-
-        <div className="buttons">
-          <PrimaryButton buttonOnClick={handleModal} textButton='Cancelar' />
-          <PrimaryButton buttonOnClick={sendData} textButton='Guardar' />
-        </div>
-      </LogInContainer>
-    </div> : <PrimaryButton buttonOnClick={handleModal} textButton='Agregar carrera' />)
+        </LogInContainer>
+      </div>
+    </>
   )
 }
