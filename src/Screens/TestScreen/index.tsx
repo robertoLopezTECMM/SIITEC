3f92b5113611c27f610ffd7d3d82d164bd22d815
Roberto Carlos Lopez Perez <roberto.lopez@tecmm.edu.mx>
2022-10-27 11:49:56 -0500

    modal to create career finished, modal to create assignature created

--- src/Screens/TestScreen/index.tsx
+++ src/Screens/TestScreen/index.tsx
@@ -4,6 +4,7 @@ import { authContext } from '../../Contexts/AuthProvider'
 import { useContext, useState } from 'react'
 import { ListItem } from '../Administrativos/administradorAcademico/components/ListItem'
 import { ModalNewCareer } from '../Administrativos/administradorAcademico/components/modalNewCareer'
+import { ModalCreateAssignature } from '../Administrativos/administradorAcademico/components/ModalCreateAssignature'
 
 export const TestScreen = () => {
   const [showModal, setShowModal] = useState(false)
@@ -14,7 +15,9 @@ export const TestScreen = () => {
 
   return(
     <div>
-      <ModalNewCareer isOpen={showModal} onCloseModal={closeModal}/>
+      {/* <ModalNewCareer isOpen={showModal} onCloseModal={closeModal}/> */}
+      <ModalCreateAssignature isOpen={showModal} onCloseModal={closeModal}/>
+
       <button onClick={()=>setShowModal(true)}>open modal</button>
     </div>
   );
