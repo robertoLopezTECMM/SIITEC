3ee4b1559904a00c258c6390e6161049ce8a02bc
Omar Israel Navarro Oliva <90788988+ImaCPU@users.noreply.github.com>
2022-10-27 13:45:41 -0500

    I updated file CheckBox component

--- src/Components/ChecksBox/ChecksBox.tsx
+++ src/Components/ChecksBox/ChecksBox.tsx
@@ -1,12 +1,40 @@
+import { ClassNames } from '@emotion/react';
 import React from 'react'
 import { Form, InputGroup } from 'react-bootstrap'
-import './ChecksBox.css'
+import { text } from 'stream/consumers';
+import {useState} from "react";
 
-export const RememberAccountCheckBox = () => {
-  return (
-    <div className='rememberAccountCheckBoxContainer'>
-        <input className='rememberAccountCheck' type='checkbox' />
-        <p className='rememberAccountText'>Recordar mi cuenta</p>
-    </div>
-  )
+import './ChecksBox.css'
+import { brotliDecompress } from 'zlib';
+import { connect } from 'http2';
+import { Console } from 'console';
+
+
+interface checkboxInput{
+  labelCheckBox: string;
+  checked?: boolean;
+  labelDirection?: string;
 }
+
+export const  RememberAccountCheckBox = ({labelCheckBox, checked, labelDirection}: checkboxInput ) => {
+  const defaultChecked = checked ? checked : false;
+  const [isChecked, setIsChecked]= useState (defaultChecked);
+  const defaultLabelDirection = labelDirection? labelDirection : false;
+  
+
+  return (
+    <div className='rememberAccountCheckBoxContainer'  >
+        <input  
+        className='rememberAccountCheckBox'
+        type='checkbox' 
+        checked={isChecked}
+        onChange={() => setIsChecked((prev) => !prev)}
+        />
+        <label className='rememberAccountText' > {labelCheckBox } </label> 
+        
+    </div>
+    
+  );
+};
+
+export default RememberAccountCheckBox
